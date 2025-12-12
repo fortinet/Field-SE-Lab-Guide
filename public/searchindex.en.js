4d43ca3833a31899b3108e26cef2de00c372fb88
Steve Sweeney <ssweeney@fortinet-us.com>
2025-12-11 22:09:17 -0600

    init setup

---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "SE How-To Build Lab",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "SE How-To Build Lab",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "SE How-To Build Lab",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
